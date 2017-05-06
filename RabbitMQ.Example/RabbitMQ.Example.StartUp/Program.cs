d567b4b6fcb3b478db095889b6819e993a096f38
Kyle Kelly <12238995+aeroradish@users.noreply.github.com>
2017-05-06 15:18:54 -0400

    Initial commit.  Basic messages, worker queues and publisher/subscriber

---
+++ RabbitMQ.Example/RabbitMQ.Example.StartUp/Program.cs
@@ -0,0 +1,99 @@
+﻿using RabbitMQ.Client;
+using RabbitMqService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Example.StartUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //OneWayMessageQueue();
+            //WorkerQueue();
+            PublishToSubscribers();
+        }
+
+        #region "OneWayMessageQueue"
+
+        public static void OneWayMessageQueue()
+        {
+            AmqpMessagingService messagingService = new AmqpMessagingService();
+            IConnection connection = messagingService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+            messagingService.SetUpQueueForOneWayMessageDemo(model);
+
+            RunOneWayMessageDemo(model, messagingService);
+           
+        }
+
+
+        private static void RunOneWayMessageDemo(IModel model, AmqpMessagingService messagingService)
+        {
+            Console.WriteLine("Enter your message and press Enter. Quit with 'q'.");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (message.ToLower() == "q") break;
+
+                messagingService.SendOneWayMessage(message, model);
+            }
+        }
+        #endregion
+
+        #region "WorkerQueue"
+        //client pulls message off queue.  message no longer available.
+
+        public static void WorkerQueue()
+        {
+            AmqpMessagingService messagingService = new AmqpMessagingService();
+            IConnection connection = messagingService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+            messagingService.SetUpQueueForWorkerQueueDemo(model);
+            RunWorkerQueueMessageDemo(model, messagingService);
+        }
+
+        private static void RunWorkerQueueMessageDemo(IModel model, AmqpMessagingService messagingService)
+        {
+            Console.WriteLine("Enter your message and press Enter. Quit with 'q'.");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (message.ToLower() == "q") break;
+                messagingService.SendMessageToWorkerQueue(message, model);
+            }
+        }
+        #endregion
+
+        #region "Publish/Subscriber"
+
+        public static void PublishToSubscribers()
+        {
+            AmqpMessagingService messagingService = new AmqpMessagingService();
+            IConnection connection = messagingService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+            messagingService.SetUpExchangeAndQueuesForDemo(model);
+
+            RunPublishSubscribeMessageDemo(model, messagingService);
+        }
+
+        private static void RunPublishSubscribeMessageDemo(IModel model, AmqpMessagingService messagingService)
+        {
+            Console.WriteLine("Enter your message and press Enter. Quit with 'q'.");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (message.ToLower() == "q") break;
+
+                messagingService.SendMessageToPublishSubscribeQueues(message, model);
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ RabbitMQ.Example/RabbitMqService/AmqpMessagingService.cs
@@ -0,0 +1,152 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.MessagePatterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMqService
+{
+    public class AmqpMessagingService
+    {
+
+        private string _hostName = "localhost";
+        private string _userName = "guest";
+        private string _password = "guest";
+        private string _exchangeName = "";
+        private bool _durable = true;
+
+        private string _oneWayMessageQueueName = "OneWayMessageQueue";
+        private string _workerQueueDemoQueueName = "WorkerQueueDemoQueue";
+        private string _publishSubscribeExchangeName = "PublishSubscribeExchange";
+        private string _publishSubscribeQueueOne = "PublishSubscribeQueueOne";
+        private string _publishSubscribeQueueTwo = "PublishSubscribeQueueTwo";
+
+        public AmqpMessagingService()
+        {
+
+        }
+
+        public IConnection GetRabbitMqConnection()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = _hostName;
+            connectionFactory.UserName = _userName;
+            connectionFactory.Password = _password;
+
+            return connectionFactory.CreateConnection();
+        }
+
+        #region "OneWayMessageQueue"
+        public void SetUpQueueForOneWayMessageDemo(IModel model)
+        {
+            //QueueDeclaration is idempotent operation 
+            //"denoting an element of a set that is unchanged in value when multiplied or otherwise operated on by itself."
+            model.QueueDeclare(_oneWayMessageQueueName, _durable, false, false, null);
+        }
+      
+        public void SendOneWayMessage(string message, IModel model)
+        {
+            IBasicProperties basicProperties = model.CreateBasicProperties();
+            basicProperties.Persistent = _durable;
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            model.BasicPublish(_exchangeName, _oneWayMessageQueueName, basicProperties, messageBytes);
+        }
+
+        public void ReceiveOneWayMessages(IModel model)
+        {
+            model.BasicQos(0, 1, false); //basic quality of service
+            QueueingBasicConsumer consumer = new QueueingBasicConsumer(model);
+            model.BasicConsume(_oneWayMessageQueueName, false, consumer);
+            while (true)
+            {
+                BasicDeliverEventArgs deliveryArguments = consumer.Queue.Dequeue() as BasicDeliverEventArgs;
+                String message = Encoding.UTF8.GetString(deliveryArguments.Body);
+                Console.WriteLine("Message received: {0}", message);
+                model.BasicAck(deliveryArguments.DeliveryTag, false);
+            }
+        }
+        #endregion
+
+        #region "WorkerQueue"
+
+        public void SetUpQueueForWorkerQueueDemo(IModel model)
+        {
+            model.QueueDeclare(_workerQueueDemoQueueName, _durable, false, false, null);
+        }
+
+        public void SendMessageToWorkerQueue(string message, IModel model)
+        {
+            IBasicProperties basicProperties = model.CreateBasicProperties();
+            basicProperties.Persistent = _durable;
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            model.BasicPublish(_exchangeName, _workerQueueDemoQueueName, basicProperties, messageBytes);
+        }
+
+        public void ReceiveWorkerQueueMessages(IModel model)
+        {
+            //Pulls message off queue, message no longer available
+
+            model.BasicQos(0, 1, false); //basic quality of service
+            QueueingBasicConsumer consumer = new QueueingBasicConsumer(model);
+            model.BasicConsume(_workerQueueDemoQueueName, false, consumer);
+            while (true)
+            {
+                BasicDeliverEventArgs deliveryArguments = consumer.Queue.Dequeue() as BasicDeliverEventArgs;
+                String message = Encoding.UTF8.GetString(deliveryArguments.Body);
+                Console.WriteLine("Message received: {0}", message);
+                model.BasicAck(deliveryArguments.DeliveryTag, false);
+            }
+        }
+        #endregion
+
+        #region "Publish/Subscriber"
+
+        public void SetUpExchangeAndQueuesForDemo(IModel model)
+        {
+            model.ExchangeDeclare(_publishSubscribeExchangeName, ExchangeType.Fanout, true);
+            model.QueueDeclare(_publishSubscribeQueueOne, true, false, false, null);
+            model.QueueDeclare(_publishSubscribeQueueTwo, true, false, false, null);
+            model.QueueBind(_publishSubscribeQueueOne, _publishSubscribeExchangeName, "");
+            model.QueueBind(_publishSubscribeQueueTwo, _publishSubscribeExchangeName, "");
+        }
+
+        public void SendMessageToPublishSubscribeQueues(string message, IModel model)
+        {
+            IBasicProperties basicProperties = model.CreateBasicProperties();
+            basicProperties.Persistent = _durable;
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            model.BasicPublish(_publishSubscribeExchangeName, "", basicProperties, messageBytes);
+        }
+
+
+        public void ReceivePublishSubscribeMessageReceiverOne(IModel model)
+        {
+            model.BasicQos(0, 1, false);
+            Subscription subscription = new Subscription(model, _publishSubscribeQueueOne, false);
+            while (true)
+            {
+                BasicDeliverEventArgs deliveryArguments = subscription.Next();
+                String message = Encoding.UTF8.GetString(deliveryArguments.Body);
+                Console.WriteLine("Message from queue: {0}", message);
+                subscription.Ack(deliveryArguments);
+            }
+        }
+
+        public void ReceivePublishSubscribeMessageReceiverTwo(IModel model)
+        {
+            model.BasicQos(0, 1, false);
+            Subscription subscription = new Subscription(model, _publishSubscribeQueueTwo, false);
+            while (true)
+            {
+                BasicDeliverEventArgs deliveryArguments = subscription.Next();
+                String message = Encoding.UTF8.GetString(deliveryArguments.Body);
+                Console.WriteLine("Message from queue: {0}", message);
+                subscription.Ack(deliveryArguments);
+            }
+        }
+        #endregion
+    }
+}
